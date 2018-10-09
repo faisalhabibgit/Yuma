4627bdec2e85274a5ed4d0e1a20100ae9c2efa4a
Faisal Habib <habib.faisal.habib@gmail.com>
2018-10-09 00:45:17 -0400

    finished setting up development environment

---
+++ backend/src/test/java/com/yuma/app/resources/AbstractRestApiTest.java
@@ -0,0 +1,12 @@
+package com.yuma.app.resources;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public abstract class AbstractRestApiTest {
+}
