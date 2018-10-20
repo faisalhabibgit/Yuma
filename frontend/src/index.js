26cd4afc84f2af4a3d67c8a56f5a82dd838fa673
nathysOne <nathys.89@gmail.com>
2018-10-20 15:17:43 -0400

    support for caterer login

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import LoginCaterer from './LoginCaterer';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<LoginCaterer />, document.getElementById('root'));
 registerServiceWorker();
