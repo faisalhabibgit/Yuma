0d8f0d9d5a396bbd095a9dc2d010e51c184d540e
yelghom <yousra.elghomari@gmail.com>
2018-11-07 18:02:28 -0500

    view consumers' preferences #7

--- backend/src/main/java/com/yuma/app/document/Consumer.java
+++ backend/src/main/java/com/yuma/app/document/Consumer.java
@@ -1,5 +1,6 @@
 package com.yuma.app.document;
 
+import java.sql.Timestamp;
 import java.util.UUID;
 
 import lombok.Getter;
@@ -17,19 +18,31 @@ public class Consumer {
 	private final UUID userId;
 	private final String firstName;
 	private final String lastName;
+	private final String personalEmail;
+	private final String workEmail;
+	private final Preferences preferences;
+	private final String timestamp;
 
-	public Consumer(UUID userId, String firstName, String lastName) {
+	public Consumer(UUID userId, String firstName, String lastName, String personalEmail, String workEmail, Preferences preferences, String timestamp) {
 		this.userId = userId;
 		this.firstName = firstName;
 		this.lastName = lastName;
+		this.personalEmail = personalEmail;
+		this.workEmail = workEmail;
+		this.preferences = preferences;
+		this.timestamp = timestamp;
 	}
 
 	@Override
 	public String toString() {
 		return "Consumer{" +
-			"userId=" + userId +
-			", firstName='" + firstName + '\'' +
-			", lastName='" + lastName + '\'' +
-			'}';
+				"userId=" + userId +
+				", firstName='" + firstName + '\'' +
+				", lastName='" + lastName + '\'' +
+				", personalEmail='" + personalEmail + '\'' +
+				", workEmail='" + workEmail + '\'' +
+				", preferences=" + preferences +
+				", timestamp=" + timestamp +
+				'}';
 	}
 }
