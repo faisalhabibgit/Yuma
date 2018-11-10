0b42a0d12142d1b47eed244db4c1756169bb8a29
yelghom <yousra.elghomari@gmail.com>
2018-11-10 09:16:02 -0500

    QueryDsl dependencies and generating QClasses

--- backend/src/main/java/com/yuma/app/repository/MealRepository.java
+++ backend/src/main/java/com/yuma/app/repository/MealRepository.java
@@ -5,7 +5,8 @@ import java.util.UUID;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.yuma.app.document.Meal;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 
-public interface MealRepository extends MongoRepository<Meal, UUID> {
+public interface MealRepository extends MongoRepository<Meal, UUID>, QueryDslPredicateExecutor<Meal> {
 
 }
