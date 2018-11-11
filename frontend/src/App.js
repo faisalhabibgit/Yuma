6d516731ef107330bbc80baf29be41d3f8c6a1c9
nathysOne <nathys.89@gmail.com>
2018-11-11 10:59:36 -0500

    #13 added registration function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import LoginCaterer from './LoginCaterer';
+import Registration from './Registration';
 
 class App extends Component {
   render() {
@@ -17,10 +18,12 @@ class App extends Component {
           
           <Link className="navbutton" to="/">Home</Link>
           <Link className="navbutton" to="/LoginCaterer">Login</Link>
+          <Link className="navbutton" to="/Registration">Registration</Link>
           <br></br>
           
           
           <Route exact path="/LoginCaterer" component={LoginCaterer} />
+          <Route exact path="/Registration" component={Registration} />
           
           
           
