7c912c83c87c474c88fb8e495c16c6a7a0c7a918
nathysOne <nathys.89@gmail.com>
2018-11-11 10:23:46 -0500

    #13 functional routing but bad layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,30 @@ import React, { Component } from 'react';
 import logo from './yuma.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import LoginCaterer from './LoginCaterer';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Yuma Ops!</h2>
+       <Router>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to Yuma Ops!</h2>
+          </div>
+          
+          <Link className="navbutton" to="/">Home</Link>
+          <Link className="navbutton" to="/LoginCaterer">Login</Link>
+          <br></br>
+          
+          
+          <Route exact path="/LoginCaterer" component={LoginCaterer} />
+          
+          
+          
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+       </Router>
     );
   }
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import LoginCaterer from './LoginCaterer';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<LoginCaterer />, document.getElementById('root'));
 registerServiceWorker();
