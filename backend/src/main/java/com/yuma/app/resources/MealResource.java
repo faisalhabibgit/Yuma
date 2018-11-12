c7f616db42ef937e22c116db2db40f8aea1bc89c
BizAhmad <ahmad.baiazid@appdirect.com>
2018-11-11 23:04:25 -0500

    updated yarn to 1.12 and fixed missing import

--- backend/src/main/java/com/yuma/app/resources/MealResource.java
+++ backend/src/main/java/com/yuma/app/resources/MealResource.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
