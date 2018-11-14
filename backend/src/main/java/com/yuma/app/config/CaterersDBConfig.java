b80b28c405d12f99f7958c35a5fe01ccf7a5bb10
BizAhmad <ahmad.baiazid@appdirect.com>
2018-11-13 21:51:16 -0500

    fix failing tests

--- backend/src/main/java/com/yuma/app/config/CaterersDBConfig.java
+++ backend/src/main/java/com/yuma/app/config/CaterersDBConfig.java
@@ -170,7 +170,7 @@ public class CaterersDBConfig {
 //		};
 //
 //	}
-}
+//}
 
 //	List<Ingredients> ingredientsList = new ArrayList<>();
 //Ingredients ingredient2 = new Ingredients("potato", 3.343, 0.02, 58.00);
@@ -187,31 +187,6 @@ public class CaterersDBConfig {
 //			catererRepository.save(new Caterer(UUID.randomUUID(), "JohnSmith", "johnsmith@gmail.com", new Address(1601, "clever", "CSL", "QC", "Canada", "H4L7I8"), "greek", new ArrayList<>(), "20190807"));
 //
 //
-//		};
-//	}
-//
-//	@Bean
-//	CommandLineRunner commandLineRunner2(MealRepository mealRepository) {
-//		logger.info("this is logging meals into the DB");
-//
-//		ingredientsList.add(ingredient1);
-//		ingredientsList.add(ingredient2);
-//		ingredientsList.add(ingredient3);
-//		ingredientsList.add(ingredient4);
-//
-//		return strings -> {
-//			mealRepository.save(new Meal(ingredientsList, UUID.randomUUID(), "chicken anshit", true, new HashSet<>()));
-//			mealRepository.save(new Meal(ingredientsList, UUID.randomUUID(), "Beef nshiet", true, new HashSet<>()));
-//
-//		};
-//	}
-//
-//	@Bean
-//	CommandLineRunner commandLineRunner3(ConsumersRepository consumersRepository) {
-//		logger.info("this is logging stuff into the DB");
-//		return strings -> {
-//			consumersRepository.save(new Consumer(UUID.randomUUID(), "Ahmad", "baiazid", "ahmad.lego@gmail.com", "ahmad.lego@work.com", new Preferences(2, true, false, new HashSet<>()), "20190807"));
-//			consumersRepository.save(new Consumer(UUID.randomUUID(), "Nate", "Lego", "nate.lego@gmail.com", "nate.lego@work.com", new Preferences(2, true, false, new HashSet<>()), "20190807"));
 //		};
 //	}
 //}
