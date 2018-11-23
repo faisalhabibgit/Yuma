1e658b9c3c75a6ab8567fc1176886d714466668e
nathysOne <nathys.89@gmail.com>
2018-11-22 20:25:14 -0500

    modified frontend structure

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './Routes';
-import App from './component/App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
