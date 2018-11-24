6f3d04bd8505ea585e74ad287e5f0b26bc7e3b7a
nathysOne <nathys.89@gmail.com>
2018-11-24 16:07:18 -0500

    functional footer and nav, but the layout is a little off...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import LoginCaterer from './component/LoginCaterer';
 import Registration from './component/Registration';
-import ForgotPassword from './component/ForgotPassword';
 import Navigation from './component/Navigation'
+import Footer from './component/Footer'
+import Error from './component/Error'
+import Home from './component/Home'
+import Test from './component/Test'
 
 class App extends Component {
-  
+
   render() {
     return (
-      <div>
-        <Navigation/>
-      </div>
+      <BrowserRouter>
+        <div>
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"></link>
+          <Navigation />
+          <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/LoginCaterer" component={LoginCaterer} />
+          <Route path="/Registration" component={Registration} />
+          <Route path="/Test" component={Test} />
+          <Route component={Error} />
+          </Switch>
+          <Footer/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- frontend/src/component/Navigation.js
+++ frontend/src/component/Navigation.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from "react-router-dom";
-
-import LoginCaterer from './LoginCaterer';
-import Registration from './Registration';
+import { Link } from "react-router-dom";
 
 import {
     Collapse,
@@ -15,73 +12,63 @@ import {
     UncontrolledDropdown,
     DropdownToggle,
     DropdownMenu,
-    DropdownItem } from 'reactstrap';
+    DropdownItem
+} from 'reactstrap';
 
 class Navigation extends Component {
 
     constructor(props) {
         super(props);
-    
+
         this.toggle = this.toggle.bind(this);
         this.state = {
-          isOpen: false
+            isOpen: false
         };
-      }
-      toggle() {
+    }
+    toggle() {
         this.setState({
-          isOpen: !this.state.isOpen
+            isOpen: !this.state.isOpen
         });
     }
-    
-    render(){
-    return (
-        <BrowserRouter>      
-<html>
-  <head>
-    <link rel="stylesheet" href= "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    </link>
-  </head>
-  <body>
-    <Navbar color="light" light expand="md">
-      <NavbarBrand href="/">Yuma</NavbarBrand>
-      <NavbarToggler onClick={this.toggle} />
-      <Collapse isOpen={this.state.isOpen} navbar>
-        <Nav className="ml-auto" navbar>
-          <NavItem>
-            <NavLink tag={Link} to="/LoginCaterer">LoginCaterer</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="https://github.com/faisalhabibgit/Yuma">GitHub</NavLink>
-          </NavItem>
-          <UncontrolledDropdown nav inNavbar>
-            <DropdownToggle nav caret>
-              Options
-            </DropdownToggle>
-            <DropdownMenu right>
-              <DropdownItem>
-                Option 1
-              </DropdownItem>
-              <DropdownItem>
-                Option 2
-              </DropdownItem>
-              <DropdownItem divider />
-              <DropdownItem>
-                Reset
-              </DropdownItem>
-            </DropdownMenu>
-          </UncontrolledDropdown>
-        </Nav>
-      </Collapse>
-    </Navbar>
-  </body>
-  
-  
-    <Route exact path="/LoginCaterer" component={LoginCaterer}/>
-    <Route path="/Registration" component={Registration}/>
-</html>
-</BrowserRouter>
-    );
-}
+
+    render() {
+        return (
+            <Navbar color="light" light expand="md">
+                <NavbarBrand href="/">Yuma</NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink tag={Link} to="/LoginCaterer">LoginCaterer</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="/Registration">Register</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://github.com/faisalhabibgit/Yuma">GitHub</NavLink>
+                        </NavItem>
+                        <UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle nav caret>
+                                Options
+                                        </DropdownToggle>
+                            <DropdownMenu right>
+                                <DropdownItem>
+                                    Option 1
+                                            </DropdownItem>
+                                <DropdownItem>
+                                    Option 2
+                                            </DropdownItem>
+                                <DropdownItem divider />
+                                <DropdownItem>
+                                    Reset
+                                            </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        );
+    }
 }
 
 export default Navigation;
