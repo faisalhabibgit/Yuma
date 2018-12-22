05b178aefa2958f2596e975168cffa20cc486f46
BizAhmad <ahmad.baiazid@appdirect.com>
2018-12-22 16:34:02 -0500

    deletes caterers logic #67

--- backend/src/main/java/com/yuma/app/util/Helper.java
+++ backend/src/main/java/com/yuma/app/util/Helper.java
@@ -4,18 +4,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-import com.yuma.app.document.Caterer;
 import com.yuma.app.document.Consumer;
 import com.yuma.app.document.Meal;
 
 public class Helper {
 
 	public static List<Meal> toMealList(final Iterable<com.yuma.app.document.Meal> iterable) {
-		return StreamSupport.stream(iterable.spliterator(), false)
-			.collect(Collectors.toList());
-	}
-
-	public static List<Caterer> toCatererList(final Iterable<com.yuma.app.document.Caterer> iterable) {
 		return StreamSupport.stream(iterable.spliterator(), false)
 			.collect(Collectors.toList());
 	}
