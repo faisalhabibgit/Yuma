f24a71b34ee73b4702b6fea2a86740da54bb1e3e
nathysOne <nathys.89@gmail.com>
2018-12-26 22:25:59 -0500

    #65 functional displaying of api req

--- frontend/src/component/Test.js
+++ frontend/src/component/Test.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 
 import Retriever from '../middleware/Retriever'
+import {
+  ListGroup, ListGroupItem,
+  Container, Col, Form, FormGroup, Label, Input
+} from 'reactstrap';
 
 class Test extends Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
+      value: '',
+      limit: 10,
       apiObject: null,
+      displayData: [],
     }
+
+    this.handleQueryChange = this.handleQueryChange.bind(this);
+    this.handleNumber = this.handleNumber.bind(this);
 
   }
 
@@ -21,11 +31,67 @@ class Test extends Component {
       });
   }
 
+  handleQueryChange(event) {
+    this.setState({ value: event.target.value });
+    this.doSearch();
+  }
+
+  handleNumber(event) {
+    this.setState({ limit: event.target.value });
+  }
+
+  doSearch() {
+    var matchedArr = [];
+    var matchCount = 0;
+    var limit = this.state.limit;
+
+    for (var i = 0; i < this.state.apiObject.length; i++) {
+      if (this.state.apiObject[i].description.toString().includes(this.state.value)
+        && (matchCount < limit)
+      ) {
+        matchedArr.push(this.state.apiObject[i].description.toString());
+        matchCount++;
+      } else if (matchCount >= limit) {
+        break;
+      }
+    }
+    this.setState({ displayData: matchedArr });
+  }
+
   render() {
-    console.log(this.state.apiObject);
+    //console.log(this.state.apiObject);
 
     return (
-      <p>Hello</p>
+      <Container>
+        <Col sm="12" md={{ size: 6, offset: 3 }}>
+          <Form className="form">
+            <FormGroup>
+              <Label>Search Meals: </Label>
+              <Input
+                type="text"
+                value={this.state.value}
+                onChange={this.handleQueryChange}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label>Result Limit: </Label>
+              <select value={this.state.limit} onChange={this.handleNumber}>
+                <option value='5'>5</option>
+                <option value='10'>10</option>
+                <option value='20'>20</option>
+                <option value='50'>50</option>
+              </select>
+            </FormGroup>
+          </Form>
+        </Col>
+        <Col sm={{ size: 6, order: 2, offset: 1 }}>
+          <ListGroup>
+            {
+              this.state.displayData.map(x => <ListGroupItem>{x}</ListGroupItem>)
+            }
+          </ListGroup>
+        </Col>
+      </Container>
     );
   }
 }
