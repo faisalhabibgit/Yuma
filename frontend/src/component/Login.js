26919c68d4ee32748e3aa68aa549121fd79da335
Giovanni <gprattico95@gmail.com>
2019-01-12 17:49:50 -0500

    Record user inputs, check for emptyness upon submission

--- frontend/src/component/Login.js
+++ frontend/src/component/Login.js
@@ -6,36 +6,78 @@ import {
   Container,
 } from 'reactstrap';
 
+//const REDIRECTDASHBOARD='/';
+
 class Login extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      email:'',
+      password:''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.postLogin = this.postLogin.bind(this);
+  }
+  
+  handleChange(event){
+    const target = event.target;
+    const value = target.value;
+    const id = target.id;
+
+    this.setState({[id]: value});
+  }
+  
+  handleSubmit(event){
+    event.preventDefault();
+    
+    if(this.state.email.length<1){
+      alert('Please enter an email address');
+    } else if(this.state.password.length<1){
+      alert('Please enter a password');
+    }else{
+      this.postLogin();
+      //this.props.history.push(REDIRECTDASHBOARD);
+    }
+  }
+  
+  postLogin(){
+    
+    console.log(this.state.email+ ' '+this.state.password);
+    //nathys add your stuff here
+    
+  }
+  
   render() {
     return (
       <Container>
         <Col sm="12" md={{ size: 6, offset: 3 }}>
           <h2>Sign In</h2>
-          <Form className="form">
-            <Col >
+          <Form className="form" onSubmit={this.handleSubmit}>
+            <Col>
               <FormGroup>
                 <Label>Email</Label>
                 <Input
                   type="email"
                   name="email"
-                  id="exampleEmail"
+                  id="email"
                   placeholder="myemail@email.com"
+                  onChange={this.handleChange}
                 />
-              </FormGroup>
-            </Col>
-            <Col>
-              <FormGroup>
-                <Label for="examplePassword">Password</Label>
+            
+                <Label>Password</Label>
                 <Input
                   type="password"
                   name="password"
-                  id="examplePassword"
-                  placeholder="********"
+                  id="password"
+                  placeholder=""
+                  onChange={this.handleChange}
                 />
               </FormGroup>
             </Col>
-            <Button>Submit</Button>
+            <Button type="submit" value="Submit">Submit</Button>
           </Form>
         </Col>
       </Container>
