cba1d599e82a83abfeb6f6b786d808e38a436212
orelmoisa <orelmoisa@gmail.com>
2019-01-18 20:39:52 -0500

    can't access Dashboard unless you are logged in #83

--- frontend/src/component/Dashboard.js
+++ frontend/src/component/Dashboard.js
@@ -1,8 +1,21 @@
 import React, {Component} from 'react';
+import ApiToken from "../middleware/ApiToken";
 
 class Dashboard extends Component {
 
 
+  constructor(props) {
+    super(props);
+
+    const apiToken = new ApiToken();
+    if (!apiToken.isAuthenticated()) {
+      console.log('Check Authentification Dashboard: FAIL');
+      this.props.history.push(`/Login`)
+    } else {
+      console.log('Check Authentification Dashboard: PASS');
+    }
+  }
+  
   render() {
     return (
       <div style={{background: '#696969'}}>
