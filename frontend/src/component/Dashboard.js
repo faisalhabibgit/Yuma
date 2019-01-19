2b8f03a037dfc0158b722a2059de7bb4f302740f
orelmoisa <orelmoisa@gmail.com>
2019-01-18 20:23:05 -0500

    added simple dashboard layout #83

--- frontend/src/component/Dashboard.js
+++ frontend/src/component/Dashboard.js
@@ -1,14 +1,76 @@
-import React, { Component } from 'react';
-import Login from "./Login";
-
+import React, {Component} from 'react';
 
 class Dashboard extends Component {
+  
 
   render() {
-    
+    return (
+      <div style={{background: '#696969'}}>
+
+        <div>
+
+          <div className="card-deck" style={{padding: '12px', height: '300px'}}>
+            <div className="card">
+              <div className="card-header">Best Meal Combo</div>
+              <div className="card-body">
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="card-deck" style={{padding: '12px', height: '300px'}}>
+            <div className="card">
+              <div className="card-header">Header</div>
+
+              <div className="card-body">
+              </div>
+
+            </div>
+            <div className="card">
+              <div className="card-header">Header</div>
+              <div className="card-body">
+              </div>
+            </div>
+            
+            <div className="card">
+              <div className="card-header">Header</div>
+              <div className="card-body">
+
+              </div>
+
+            </div>
+          </div>
+
+
+        </div>
+
+
+        <div>
+          <div className="card-deck" style={{padding: '12px', height: '200px'}}>
+            <div className="card">
+              <div className="card-header">Header</div>
+              <div className="card-body">
+              </div>
+            </div>
+            <div className="card">
+              <div className="card-header">Header</div>
+              <div className="card-body">
+              </div>
+            </div>
+
+          </div>
+
+
+        </div>
+
+      </div>
+
+
+    );
+
   }
-  
-  
 }
+
 
 export default Dashboard;
