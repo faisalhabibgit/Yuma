07f3b5fd6c397928f327e8c52a84b5b26412e4ce
nathysOne <nathys.89@gmail.com>
2019-01-23 18:32:25 -0500

    #102 add displayUser interface to the dashboard layout

--- frontend/src/component/Dashboard.js
+++ frontend/src/component/Dashboard.js
@@ -1,9 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ApiToken from "../middleware/ApiToken";
 
+import DisplayUsers from "./DisplayUsers";
+
 import {
-  Card, CardHeader,  CardBody,
-   CardDeck
+  Card, CardHeader, CardBody,
+  CardDeck
 } from 'reactstrap';
 
 class Dashboard extends Component {
@@ -23,88 +25,89 @@ class Dashboard extends Component {
 
   render() {
     return (
-      <div style={{background: '#ADB7BF'}}>
-       
-        <CardDeck style={{padding:'12px', height:'300px'}}>
-            <Card>
-              <CardHeader  className="text-center" style={{background: '#B9C5D5'}}>
-                <h5 style={{color: 'black', fontFamily: 'Comic Sans MS'}}> Meal Combo</h5>
-              </CardHeader>
-              <CardBody>
-              </CardBody>
-            </Card>
-          
-        </CardDeck>
+      <div style={{ background: '#ADB7BF' }}>
 
-
-        <CardDeck style={{padding:'12px', height:'450px'}}>
-            <Card>
-              <CardHeader  className="text-center" style={{background: '#B9C5D5'}}>
-                <h5 style={{color: 'black', fontFamily: 'Comic Sans MS'}}> Available Meals</h5>
-              </CardHeader>
-              <CardBody>
-              </CardBody>
-            </Card>
-
+        <CardDeck style={{ padding: '12px', height: '300px' }}>
           <Card>
-            <CardHeader  className="text-center" style={{background: '#B9C5D5'}}>
-              <h5 style={{color: 'black', fontFamily: 'Comic Sans MS'}}> Active Users</h5>
+            <CardHeader className="text-center" style={{ background: '#B9C5D5' }}>
+              <h5 style={{ color: 'black', fontFamily: 'Comic Sans MS' }}> Meal Combo</h5>
             </CardHeader>
             <CardBody>
             </CardBody>
           </Card>
-       
+
         </CardDeck>
 
 
-        <CardDeck style={{padding:'12px', height:'250px'}}>
-            <Card>
-              <CardHeader  className="text-center" style={{background: '#B9C5D5'}}>
-                <h5 style={{color: 'black', fontFamily: 'Comic Sans MS'}}> Header</h5>
-              </CardHeader>
-              <CardBody>
-              </CardBody>
-            </Card>
-
+        <CardDeck style={{ padding: '12px', height: '450px' }}>
           <Card>
-            <CardHeader  className="text-center" style={{background: '#B9C5D5'}}>
-              <h5 style={{color: 'black', fontFamily: 'Comic Sans MS'}}> Header</h5>
+            <CardHeader className="text-center" style={{ background: '#B9C5D5' }}>
+              <h5 style={{ color: 'black', fontFamily: 'Comic Sans MS' }}> Available Meals</h5>
             </CardHeader>
             <CardBody>
             </CardBody>
           </Card>
 
           <Card>
-            <CardHeader  className="text-center" style={{background: '#B9C5D5'}}>
-              <h5 style={{color: 'black', fontFamily: 'Comic Sans MS'}}> Header</h5>
+            <CardHeader className="text-center" style={{ background: '#B9C5D5' }}>
+              <h5 style={{ color: 'black', fontFamily: 'Comic Sans MS' }}> Active Users</h5>
+            </CardHeader>
+            <CardBody>
+              <DisplayUsers />
+            </CardBody>
+          </Card>
+
+        </CardDeck>
+
+
+        <CardDeck style={{ padding: '12px', height: '250px' }}>
+          <Card>
+            <CardHeader className="text-center" style={{ background: '#B9C5D5' }}>
+              <h5 style={{ color: 'black', fontFamily: 'Comic Sans MS' }}> Header</h5>
             </CardHeader>
             <CardBody>
             </CardBody>
           </Card>
-            
+
+          <Card>
+            <CardHeader className="text-center" style={{ background: '#B9C5D5' }}>
+              <h5 style={{ color: 'black', fontFamily: 'Comic Sans MS' }}> Header</h5>
+            </CardHeader>
+            <CardBody>
+            </CardBody>
+          </Card>
+
+          <Card>
+            <CardHeader className="text-center" style={{ background: '#B9C5D5' }}>
+              <h5 style={{ color: 'black', fontFamily: 'Comic Sans MS' }}> Header</h5>
+            </CardHeader>
+            <CardBody>
+            </CardBody>
+          </Card>
+
         </CardDeck>
-        
-        
-      <CardDeck style={{padding:'12px', height:'275px'}}>
-            <Card>
-              <CardHeader  className="text-center" style={{background: '#B9C5D5'}}>
-                <h5 style={{color: 'black', fontFamily: 'Comic Sans MS'}}> Header</h5>
-              </CardHeader>
-              <CardBody>
-              </CardBody>
-            </Card>
-         
-          
-            <Card>
-              <CardHeader  className="text-center" style={{background: '#B9C5D5'}}>
-                <h5 style={{color: 'black', fontFamily: 'Comic Sans MS'}}>Header</h5>
-              </CardHeader>
-              <CardBody>
-              </CardBody>
-            </Card>
- 
-      </CardDeck>
-     
+
+
+        <CardDeck style={{ padding: '12px', height: '275px' }}>
+          <Card>
+            <CardHeader className="text-center" style={{ background: '#B9C5D5' }}>
+              <h5 style={{ color: 'black', fontFamily: 'Comic Sans MS' }}> Header</h5>
+            </CardHeader>
+            <CardBody>
+            </CardBody>
+          </Card>
+
+
+          <Card>
+            <CardHeader className="text-center" style={{ background: '#B9C5D5' }}>
+              <h5 style={{ color: 'black', fontFamily: 'Comic Sans MS' }}>Header</h5>
+            </CardHeader>
+            <CardBody>
+            </CardBody>
+          </Card>
+
+        </CardDeck>
+
       </div>
     );
 
---
+++ frontend/src/component/DisplayUsers.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class DisplayUsers extends Component {
+
+    render() {
+        return (
+            <span>
+                <h1>THIS DISPLAYS USERS!!!</h1>
+            </span>
+        );
+    }
+}
+
+export default DisplayUsers;
