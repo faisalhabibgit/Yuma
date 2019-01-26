8de1cd049c0578394ab1e01ee2c018cd35d4bf02
orelmoisa <orelmoisa@gmail.com>
2019-01-26 15:56:01 -0500

    basic layout  of display meals, still need to add styling #106

--- frontend/src/component/AvailableMeals.js
+++ frontend/src/component/AvailableMeals.js
@@ -1,14 +1,62 @@
 import React, { Component } from 'react';
+import Retriever from '../middleware/Retriever';
 
-class AvailabeMeals extends Component {
+import ApiToken from "../middleware/ApiToken";
 
-  render() {
+
+
+class AvailableMeals extends Component {
+
+
+  constructor(props) {
+    super(props);
+
+    const apiToken = new ApiToken();
+    if(!apiToken.isAuthenticated()){
+      console.log('User Not Logged');
+      this.props.history.push(`/Login`)
+    }else{
+      console.log('User Login Success');
+    }
+
+    this.state = {
+    
+      Meals: {}
+    }
+
+    
+  }
+
+  componentDidMount() {
+    const retriever = new Retriever('api/meals/availablemeals');
+    retriever.getEntityPromise()
+      .then((obj) => {
+        this.setState({ Meals: obj });
+      })
+  }
+ 
+  doSearch() {
+    var matchedArr = [];
+
+    for (var i = 0; i < this.state.Meals.length; i++) {
+      {
+        matchedArr.push(this.state.Meals[i].description.toString());
+      }
+    }
+    return matchedArr
+  }
+ 
+  render () {
+    
+    var Meals = this.doSearch()
+   
     return (
-      <span>
-		                <h1>Meals</h1>
-      </span>
+      
+      <div> {Meals} </div>
     );
   }
 }
+  
 
-export default AvailabeMeals;
+
+export default AvailableMeals;
