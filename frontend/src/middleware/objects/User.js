ba49a58e03ba258ae23e4f99705c444b63b41d9b
nathysOne <nathys.89@gmail.com>
2019-01-28 18:37:11 -0500

    #111 added setters for User entity

--- frontend/src/middleware/objects/User.js
+++ frontend/src/middleware/objects/User.js
@@ -1,25 +1,32 @@
-class User{
-  constructor(userId, firstName, lastName, email, plan, isActive, timestamp, password, ) {
+class User {
+  constructor() {
 
-  this.userId = userId;
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.email = email;
-  this.plan = plan;
-  this.isActive = isActive;
-  this.timestamp = timestamp;
-  this.password = password;
-  
-  
-  this.dislikesList = dislikesList;
-    }
-    
-    toString(){
-     return (this.userId + this.firstName + this.lastName + 
-             this.email +this.password + this.isActive + this.plan + this.dislikesList);
+    this.userId = userId;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.plan = plan;
+    this.isActive = isActive;
+    this.timestamp = timestamp;
+
+
+    this.dislikesList = dislikesList;
+  }
+
+  setUserId(userId) { this.userId = userId }
+  setFirstName(firstName) { this.firstName = firstName }
+  setLastName(lastName) { this.lastName = lastName }
+  setEmail(email) { this.email = email }
+  setPlan(plan) { this.plan = plan }
+  setIsActive(isActive) { this.isActive = isActive }
+  setTimestamp(timestamp) { this.timestamp = timestamp }
+
+  toString() {
+    return (this.userId + this.firstName + this.lastName +
+      this.email + this.password + this.isActive + this.plan + this.dislikesList);
   }
 }
 
 export default User;
 
- 
+
