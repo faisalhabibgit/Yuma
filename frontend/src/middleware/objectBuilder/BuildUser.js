ea16bc0c0210de42ed926322dadf7029e25f6175
nathysOne <nathys.89@gmail.com>
2019-01-28 19:01:18 -0500

    #111 created builder for user and updated retriever

---
+++ frontend/src/middleware/objectBuilder/BuildUser.js
@@ -0,0 +1,59 @@
+import ApiToken from '../ApiToken';
+import User from '../objects/User';
+
+
+class BuildUser {
+
+    constructor() {
+        console.log("Building User...");
+    }
+
+    getUserPromiseObj(apiPath) {
+        var userList = [];
+        var api;
+        const apiToken = new ApiToken();
+
+        var obj = {
+            method: 'GET',
+            headers: {
+                'cache-control': "no-cache",
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + apiToken.getCookie('yuma-token')
+            }
+        }
+
+        return fetch(api, obj)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Server response wasn\'t OK');
+                }
+            })
+            .then((myJson) => {
+                for (var i = 0; i < myJson.length; i++) {
+                    var obj = myJson[i];
+
+                    var user = new User();
+                    user.setName(obj.firstName);
+
+                    user.setUserId(obj.userId);
+                    user.setFirstName(obj.firstName);
+                    user.setLastName(obj.lastName);
+                    user.setEmail(obj.email);
+                    user.setPlan(obj.plan);
+                    user.setIsActive(obj.enabled);
+                    user.setTimestamp(obj.timestamp);
+                    user.setMealList(obj.mealList); //TODO: change from JSON obj to Entity obj
+                    user.setDislikesList(obj.dislikesList);
+
+                    userList.push(user);
+                }
+                return userList;
+            });
+    }
+
+
+}
+
+export default BuildUser;
--- frontend/src/middleware/objects/User.js
+++ frontend/src/middleware/objects/User.js
@@ -1,16 +1,16 @@
 class User {
   constructor() {
 
-    this.userId = userId;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.email = email;
-    this.plan = plan;
-    this.isActive = isActive;
-    this.timestamp = timestamp;
+    this.userId = null;
+    this.firstName = null;
+    this.lastName = null;
+    this.email = null;
+    this.plan = null;
+    this.isActive = null;
+    this.timestamp = null;
 
-
-    this.dislikesList = dislikesList;
+    this.mealList = null;
+    this.dislikesList = null;
   }
 
   setUserId(userId) { this.userId = userId }
@@ -20,6 +20,8 @@ class User {
   setPlan(plan) { this.plan = plan }
   setIsActive(isActive) { this.isActive = isActive }
   setTimestamp(timestamp) { this.timestamp = timestamp }
+  setMealList(mealList){this.mealList = mealList;}
+  setDislikesList(dislikesList){this.dislikesList = dislikesList}
 
   toString() {
     return (this.userId + this.firstName + this.lastName +
