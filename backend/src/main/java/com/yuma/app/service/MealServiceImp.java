202ca83a2d1fbf832e9cc2cd87e681cd0ed32b33
hamidehAziz <h_azizmohamadi@yahoo.com>
2019-01-30 21:46:59 -0500

    interface for MealService is added #112

---
+++ backend/src/main/java/com/yuma/app/service/MealServiceImp.java
@@ -0,0 +1,56 @@
+package com.yuma.app.service;
+
+import com.yuma.app.document.Meal;
+import com.yuma.app.repository.MealRepository;
+import com.yuma.app.repository.UserRepository;
+import com.yuma.app.to.MealTO;
+import org.springframework.core.convert.ConversionService;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface MealServiceImp {
+
+
+	public MealService(MealRepository mealRepository, UserRepository userRepository, ConversionService conversionService) {
+
+	}
+
+	public List<MealTO> list() {
+		
+	}
+
+	public MealTO update(MealTO mealTo) {
+		
+	}
+
+	public MealTO create(MealTO mealTo) {
+		
+	}
+
+	public List<MealTO> weeklyCombo() {
+		
+	}
+
+	public List<MealTO> availableMeals(){
+		
+	}
+
+	public void deleteMeal(UUID mealId) {
+		
+		
+	}
+
+	public MealTO findByDescription(String description) {
+		
+	}
+
+	protected List<MealTO> convertMealToMealTO(List<Meal> meals){
+
+
+
+	
+
+	
+
+
---
+++ backend/src/main/java/com/yuma/app/service/UserServiceImp.java
@@ -0,0 +1,4 @@
+package com.yuma.app.service;
+
+public interface UserServiceImp {
+}
