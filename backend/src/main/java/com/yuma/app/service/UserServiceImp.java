d906c5128217eb77f17486be717f4f2ed7043db7
hamidehAziz <h_azizmohamadi@yahoo.com>
2019-01-30 21:53:10 -0500

    UserService interface is added. #112

--- backend/src/main/java/com/yuma/app/service/UserServiceImp.java
+++ backend/src/main/java/com/yuma/app/service/UserServiceImp.java
@@ -1,4 +1,63 @@
 package com.yuma.app.service;
 
+import com.yuma.app.document.User;
+import com.yuma.app.payload.SignUpRequest;
+import com.yuma.app.repository.RoleRepository;
+import com.yuma.app.repository.UserRepository;
+import com.yuma.app.to.UserTO;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+
 public interface UserServiceImp {
-}
+	public UserService(UserRepository userRepository, ConversionService conversionService, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+
+
+	}
+
+	public User saveUser(SignUpRequest req) {
+		
+	}
+
+	public List<UserTO> list() {
+		
+	}
+
+	public UserTO findUserByEmail(String email) {
+		
+	}
+
+	public void deleteUserByUserID(String uuid){
+		
+	}
+
+	public UserTO create(UserTO userTO) {
+		
+	}
+
+	public UserTO updateUser(UserTO userTO){
+		
+	}
+
+	public boolean existsByEmail(String email){
+		
+	}
+
+	public List<UserTO> activeUsers(){
+		
+	}
+	protected List<UserTO> convertUserListToUserTOList(List<User> userList){
+		
+	}
+
+	
+
+
+
+
+
+
+
+
+	}
