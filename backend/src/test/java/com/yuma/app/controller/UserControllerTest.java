1a0e2de9587f772a3e3ffdae5b937884856db7a5
Giovanni <gprattico95@gmail.com>
2019-03-16 02:47:17 -0400

    Add unit test for userController #141

--- backend/src/test/java/com/yuma/app/controller/UserControllerTest.java
+++ backend/src/test/java/com/yuma/app/controller/UserControllerTest.java
@@ -5,12 +5,14 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import com.yuma.app.service.ConsumerService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.yuma.app.to.UserTO;
@@ -52,6 +54,19 @@ public class UserControllerTest {
 		when(userService.updateUser(userTO)).thenReturn(userTO);
 		userController.updateUser(userTO);
 		verify(userService).updateUser(userTO);
+	}
+	
+	@Test
+	public void testGetByCompany(){
+		
+		ArrayList<UserTO> actualList = new ArrayList<UserTO>(Arrays.asList(Mockito.mock(UserTO.class),Mockito.mock(UserTO.class)));
+		
+		when(userService.findUsersByCompany("Google")).thenReturn(actualList);
+		
+		//call our API
+		userController.getUsersByCompany("Google");
+		verify(userService).findUsersByCompany("Google");
+		
 	}
 
 	private UserTO prepareAndReturnUserTo() {
