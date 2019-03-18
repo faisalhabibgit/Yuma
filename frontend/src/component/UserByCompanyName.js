78dacede3f889b0f034ec270b3205ba17c91b4c6
Lordofsnails <yanming_hu1996@yahoo.ca>
2019-03-18 00:17:53 -0400

    #142 fixing the search function and api call

--- frontend/src/component/UserByCompanyName.js
+++ frontend/src/component/UserByCompanyName.js
@@ -3,7 +3,9 @@ import {
   ListGroup, ListGroupItem,
   Container, Col, Form, FormGroup, Label, Input
 } from 'reactstrap';
+import ApiToken from '../middleware/ApiToken';
 
+const API = "api/rest/company/"
 
 class UserByCompanyName extends Component {
 
@@ -12,17 +14,57 @@ class UserByCompanyName extends Component {
 
     this.state = {
       userInput: "",
-      searchList: ["mock list",
-                  "some stuff"]
+      apiObject: null,
+      searchList: [],
     }
 
     this.handleQueryChange = this.handleQueryChange.bind(this);
+    this.companyFetch = this.companyFetch.bind(this);
+  }
+
+  checkAuthenticated(){
+    const apiToken = new ApiToken();
+    if(!apiToken.isAuthenticated()){
+      console.log('User Not Logged');
+      this.props.history.push(`/Login`)
+    }else{
+      console.log('User Login Success');
+    }
   }
 
   handleQueryChange(input){
     this.setState({
       userInput: input
-    }, ()=>console.log(input))
+    }, this.companyFetch(input));
+    if( (this.state.apiObject === null) || (this.searchList = ["no result"])) {
+      this.setState({
+        searchList: ["no result"]
+      })
+    }
+    else{
+      this.search()
+    }
+  }
+
+  companyFetch(company){
+    console.log(company)
+    fetch(API + company)
+    .then((obj) => {
+      this.setState({ apiObject: obj})
+    })
+  }
+
+  search() {
+    var matchedArr = [];
+    for (var i = 0; i < this.state.apiObject.length; i++) {
+      if (this.state.apiObject[i].company.toString().includes(this.state.value)
+      ) {
+        matchedArr.push("email: "  + this.state.apiObject[i].email.toString() +
+                        "First Name: " + this.state.apiObject[i].firstname.toString() +
+                        "Last Name: " + this.state.apiObject[i].lastname.toString());
+      }
+    }
+    this.setState({ searchList: matchedArr });
   }
 
   render() {
