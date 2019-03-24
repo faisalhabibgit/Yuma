7819a99f71ce4c9618f72dc200bfef2a1be4b2da
BizAhmad <ahmad.baiazid@appdirect.com>
2019-03-24 19:36:43 -0400

    refactor and finalized fetching users from Yuma servers #115

--- backend/src/main/java/com/yuma/app/updater/UpdateUsersService.java
+++ backend/src/main/java/com/yuma/app/updater/UpdateUsersService.java
@@ -3,6 +3,7 @@ package com.yuma.app.updater;
 import static org.springframework.util.CollectionUtils.isEmpty;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,83 +19,132 @@ import okhttp3.Response;
 public class UpdateUsersService {
 
 	private static HashMap<String, YumaPreferencePOJO> usersCovered = new HashMap<>();
-
 	private static final String endpoint = "http://itsyuma.com/preferences";
 
-
-	public static void main(String[] args){
-		fetchYumaUsers();
+	public void fetchYumaUsers(){
+		BufferedReader responseBufferedReader = makeCall();
+		JSONArray jsonArray = getJsonArray(responseBufferedReader);
+		parseThroughPreferences(jsonArray);
 	}
 
-	public static void fetchYumaUsers(){
-		OkHttpClient client = new OkHttpClient();
+	private JSONArray getJsonArray(BufferedReader responseBufferedReader) {
 		JSONParser jsonParser = new JSONParser();
 
-		Request request = new Request.Builder()
-			.url("http://itsyuma.com/preferences")
-			.get()
-			.addHeader("Content-Type", "application/json")
-			.addHeader("cache-control", "no-cache")
-			.addHeader("Postman-Token", "eda36bdd-ef2e-4a77-8cad-c0a78224a8cb")
-			.build();
-
 		try {
-			Response response = client.newCall(request).execute();
-			if (response.code() == 200) {
-				BufferedReader br = new BufferedReader(new InputStreamReader(response.body().byteStream()));
-				Object obj = jsonParser.parse(br);
+			Object obj = jsonParser.parse(responseBufferedReader);
+			JSONObject jsonObject = (JSONObject) obj;
+			return (JSONArray) jsonObject.get("data");
 
-				JSONObject jsonObject = (JSONObject) obj;
-				JSONArray jsonArray = (JSONArray) jsonObject.get("data");
-				
-				for (Object jsonArrayIndex: jsonArray){
-					JSONObject preferenceJsonObject = (JSONObject) jsonArrayIndex;
-					JSONObject userJsonObject = (JSONObject) preferenceJsonObject.get("user");
-					JSONObject groupJsonObject = (JSONObject) userJsonObject.get("group");
-					String userId = userJsonObject.get("_id").toString();
-					if (usersCovered.containsKey(userId)){
-						YumaPreferencePOJO yumaPreferencePOJO = usersCovered.get(userId);
-						String preferenceKeyType = preferenceJsonObject.get("key").toString();
-						String value = preferenceJsonObject.get("value").toString();
-						switch (preferenceKeyType) {
-							case "general":
-								yumaPreferencePOJO.getGeneralPreference().add(value);
-								break;case "diet": yumaPreferencePOJO.getDiet().add(value);
-								break;case "allergy": yumaPreferencePOJO.getAllergies().add(value);
-								break;case "addition": yumaPreferencePOJO.getAdditions().add(value);
-								break;case "other": yumaPreferencePOJO.getOther().add(value);
-								break;case "plan": yumaPreferencePOJO.setNumberOfMealsPerWeek(Integer.parseInt(value)); } } else {
-						if (!isEmpty(groupJsonObject)) {
-							YumaGroupPOJO yumaGroupPOJO = YumaGroupPOJO.builder()
-								.alias(groupJsonObject.get("alias").toString())
-								.build();
-							YumaUserPOJO yumaUserPOJO = YumaUserPOJO.builder()
-								.id(userId)
-								.firstName(userJsonObject.get("firstName").toString())
-								.lastName(userJsonObject.get("lastName").toString())
-								.yumaGroupPOJO(yumaGroupPOJO)
-								.build();
-							YumaPreferencePOJO yumaPreferencePOJO = YumaPreferencePOJO.builder()
-								.additions(new ArrayList<>())
-								.allergies(new ArrayList<>())
-								.diet(new ArrayList<>())
-								.generalPreference(new ArrayList<>())
-								.other(new ArrayList<>())
-								.user(yumaUserPOJO)
-								.build();
-							usersCovered.put(userId,yumaPreferencePOJO); } } }int num=0;
-				for (YumaPreferencePOJO yumaPreferencePOJO: usersCovered.values()){
-					System.out.print(yumaPreferencePOJO.getNumberOfMealsPerWeek()+",");
-					System.out.print(yumaPreferencePOJO.getUser().getFirstName() +",");
-					System.out.print(yumaPreferencePOJO.getUser().getLastName() +",");
-					System.out.println();
-					num++;
-				}
-				System.out.println(num);
-			}
 		}catch (Exception e){
 			e.printStackTrace();
+			return null;
 		}
+	}
 
+	private BufferedReader makeCall(){
+		OkHttpClient client = new OkHttpClient();
+		try {
+			Response response = client.newCall(setupRequest()).execute();
+			return new BufferedReader(new InputStreamReader(response.body().byteStream()));
+		}
+		catch (IOException e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private Request setupRequest(){
+		return new Request.Builder()
+			.url(endpoint)
+			.get()
+			.addHeader("Content-Type", "application/json")
+			.build();
+	}
+
+	protected void parseThroughPreferences(JSONArray preferencesArray){
+		for (Object jsonObject : preferencesArray){
+			JSONObject preferenceJsonObject = (JSONObject) jsonObject;
+			JSONObject userJsonObject = (JSONObject) preferenceJsonObject.get("user");
+			String userId = userJsonObject.get("_id").toString();
+			
+			if (usersCovered.containsKey(userId)){
+				YumaPreferencePOJO yumaPreferencePOJO = usersCovered.get(userId);
+				addPreferenceToExistingPreferencePOJO(yumaPreferencePOJO, preferenceJsonObject);
+			}
+			else if(checkIfUserHasGroup(userJsonObject)) {
+				saveToHashMap(serializeObjects(userJsonObject));
+			}
+		}
+	}
+
+	private boolean checkIfUserHasGroup(JSONObject userJsonObject) {
+		return !isEmpty(parseGroupJsonObject(userJsonObject));
+	}
+
+	private JSONObject parseGroupJsonObject(JSONObject userJsonObject){
+		return (JSONObject) userJsonObject.get("group");
+	}
+
+	private void addPreferenceToExistingPreferencePOJO(YumaPreferencePOJO yumaPreferencePOJO, JSONObject preferenceJsonObject) {
+		String preferenceKeyType = preferenceJsonObject.get("key").toString();
+		String value = preferenceJsonObject.get("value").toString();
+		switch (preferenceKeyType) {
+			case "general":
+				yumaPreferencePOJO.getGeneralPreference().add(value);
+				break;
+			case "diet":
+				yumaPreferencePOJO.getDiet().add(value);
+				break;
+			case "allergy":
+				yumaPreferencePOJO.getAllergies().add(value);
+				break;
+			case "addition":
+				yumaPreferencePOJO.getAdditions().add(value);
+				break;
+			case "other":
+				yumaPreferencePOJO.getOther().add(value);
+				break;
+			case "plan":
+				yumaPreferencePOJO.setNumberOfMealsPerWeek(Integer.parseInt(value));
+		}
+	}
+
+	private YumaPreferencePOJO serializeObjects(JSONObject userJsonObject) {
+		YumaGroupPOJO yumaGroupPOJO = serializeYumaGroupPOJO(parseGroupJsonObject(userJsonObject));
+		YumaUserPOJO yumaUserPOJO = serializeYumaUserGroupPOJO(userJsonObject);
+		yumaUserPOJO.setYumaGroupPOJO(yumaGroupPOJO);
+
+		return serializeYumaPreferencePOJO(yumaUserPOJO);
+	}
+
+	private YumaGroupPOJO serializeYumaGroupPOJO(JSONObject groupJsonObject) {
+		return YumaGroupPOJO.builder()
+			.id(groupJsonObject.get("_id").toString())
+			.alias(groupJsonObject.get("alias").toString())
+			.build();
+	}
+	
+	private YumaUserPOJO serializeYumaUserGroupPOJO(JSONObject userJsonObject) {
+		return YumaUserPOJO.builder()
+			.id(userJsonObject.get("_id").toString())
+			.firstName(userJsonObject.get("firstName").toString())
+			.lastName(userJsonObject.get("lastName").toString())
+			.build();
+	}
+
+	private YumaPreferencePOJO serializeYumaPreferencePOJO(YumaUserPOJO yumaUserPOJO) {
+		return YumaPreferencePOJO.builder()
+			.user(yumaUserPOJO)
+			.additions(new ArrayList<>())
+			.allergies(new ArrayList<>())
+			.diet(new ArrayList<>())
+			.generalPreference(new ArrayList<>())
+			.other(new ArrayList<>())
+			.build();
+	}
+
+	private void saveToHashMap(YumaPreferencePOJO yumaPreferencePOJO) {
+		YumaUserPOJO yumaUserPOJO = yumaPreferencePOJO.getUser();
+		usersCovered.put(yumaUserPOJO.getId(), yumaPreferencePOJO);
 	}
 }
