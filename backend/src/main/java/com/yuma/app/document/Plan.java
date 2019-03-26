d02a9670fc3c31ff1cb62b137d85b4eb6aba0dcc
BizAhmad <ahmad.baiazid@appdirect.com>
2019-03-25 22:00:48 -0400

    #115 fixed some of the combo meal

--- backend/src/main/java/com/yuma/app/document/Plan.java
+++ backend/src/main/java/com/yuma/app/document/Plan.java
@@ -5,7 +5,9 @@ import java.util.Set;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -16,6 +18,8 @@ import com.yuma.app.document.enums.SpecialRequest;
 @Document
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 @Builder
 public class Plan {
 	private int numOfMeals;
