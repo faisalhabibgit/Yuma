e1177f4b30104939e05c492710a34f5240116395
yelghom <yousra.elghomari@gmail.com>
2019-04-07 00:47:16 +0200

    sort the enum map from the highest score percentage to lowest score #172

--- backend/src/main/java/com/yuma/app/service/CombinationScoreService.java
+++ backend/src/main/java/com/yuma/app/service/CombinationScoreService.java
@@ -1,9 +1,18 @@
 package com.yuma.app.service;
 
+import com.yuma.app.document.enums.ProteinType;
 import com.yuma.app.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Map.Entry.comparingByValue;
+import static java.util.stream.Collectors.toMap;
 
 @Slf4j
 @Service
@@ -16,4 +25,15 @@ public class CombinationScoreService {
 		this.userRepository = userRepository;
 	}
 
+	public Map<ProteinType, Double> highestOrder(EnumMap<ProteinType, Double> combination) {
+		Map<ProteinType, Double> sorted;
+		sorted = combination
+			.entrySet()
+			.stream()
+			.sorted(Collections.reverseOrder(comparingByValue()))
+			.collect(
+				toMap(e -> e.getKey(), e -> e.getValue(), (e1, e2) -> e2,
+					HashMap::new));
+		return sorted;
+	}
 }
