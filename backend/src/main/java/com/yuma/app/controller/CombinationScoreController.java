c9a2381b6882a1f5e1e13c6b817eff36a7bcf254
yelghom <yousra.elghomari@gmail.com>
2019-04-07 00:59:07 +0200

    Add GET API to fetch best protein type combination for a user

---
+++ backend/src/main/java/com/yuma/app/controller/CombinationScoreController.java
@@ -0,0 +1,32 @@
+package com.yuma.app.controller;
+
+import com.yuma.app.document.enums.ProteinType;
+import com.yuma.app.service.CombinationScoreService;
+import com.yuma.app.service.ProteinTypeScoringService;
+import com.yuma.app.to.ConsumerTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.EnumMap;
+
+@Slf4j
+@RestController
+@RequestMapping("api/combination")
+public class CombinationScoreController {
+	private CombinationScoreService combinationScoreService;
+	private ProteinTypeScoringService proteinTypeScoringService;
+
+	public CombinationScoreController(CombinationScoreService combinationScoreService, ProteinTypeScoringService proteinTypeScoringService) {
+		this.combinationScoreService = combinationScoreService;
+		this.proteinTypeScoringService = proteinTypeScoringService;
+	}
+
+	@GetMapping("/user/{user}")
+	public EnumMap<ProteinType, Double> getProteinPercentage(@PathVariable ConsumerTO consumerTO) {
+		log.info("fetching best protein combination for user");
+		return combinationScoreService.bestCombinationForUser(proteinTypeScoringService.calculateProteinTypeScore(), consumerTO);
+	}
+}
