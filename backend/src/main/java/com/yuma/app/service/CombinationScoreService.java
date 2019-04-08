6c71269649ba08426143736a51ca567fa7ae60cf
yelghom <yousra.elghomari@gmail.com>
2019-04-08 19:34:53 +0200

    get Health Label from the user #188

--- backend/src/main/java/com/yuma/app/service/CombinationScoreService.java
+++ backend/src/main/java/com/yuma/app/service/CombinationScoreService.java
@@ -41,18 +41,26 @@ public class CombinationScoreService {
 		return topCombo;
 	}
 	
-	public HealthLabels convertAllergensToHealthLabels(Allergens allergens){
-		switch (allergens){
-			case DAIRY: return HealthLabels.DAIRY_FREE;
-			case GLUTEN:return HealthLabels.GLUTEN_FREE;
-			case PEANUT:return HealthLabels.PEANUT_FREE;
-			case SHELLFISH: return HealthLabels.SHELLFISH_FREE;
-			case SOY:return HealthLabels.SOY_FREE;
-			case TREE_NUTS:return HealthLabels.TREE_NUT_FREE;
-			default:  return HealthLabels.NONE;
-		}
-		
+	public Set<HealthLabels> getHealthLabelsFromUser(Consumer consumer){
+		return convertAllergensToHealthLabels(consumer.getAllergies());
 	}
+	
+	public Set<HealthLabels> convertAllergensToHealthLabels(Set<Allergens> allergens){
+		Set<HealthLabels> healthLabels = new HashSet<>();
+		for(Allergens allergies: allergens){
+			switch (allergies){
+				case DAIRY: healthLabels.add(HealthLabels.DAIRY_FREE);
+				case GLUTEN: healthLabels.add(HealthLabels.GLUTEN_FREE);
+				case PEANUT: healthLabels.add(HealthLabels.PEANUT_FREE);
+				case SHELLFISH: healthLabels.add(HealthLabels.SHELLFISH_FREE);
+				case SOY: healthLabels.add(HealthLabels.SOY_FREE);
+				case TREE_NUTS: healthLabels.add(HealthLabels.TREE_NUT_FREE);
+				default:  healthLabels.add(HealthLabels.NONE);
+			}
+		}
+		return healthLabels;
+	}
+	
 	private EnumMap<ProteinType, Double> topCombination(int numOfmeals, Map<ProteinType, Double> proteinTypeMap ){
 		EnumMap<ProteinType, Double> proteinTypeEnumMap = new EnumMap<>(ProteinType.class);
 		for (int i = 0; i < numOfmeals; i++) {
