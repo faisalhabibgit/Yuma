fadceee09be2eedc5dc8b312ce2ddd132734cb70
yelghom <yousra.elghomari@gmail.com>
2019-04-11 23:51:48 +0200

    remove unnecessary space

--- frontend/src/component/Delivery.js
+++ frontend/src/component/Delivery.js
@@ -10,38 +10,20 @@ import Retriever from '../middleware/Retriever';
 
 class Delivery extends Component{
 
-    // constructor(props) {
-    //     super(props);
-
-    //     this.state = {
-
-    //     };
-
-    //     this.handleSubmit = this.handleSubmit.bind(this);
-
 render() {
       
     return(
       <Container>
-
         <CardDeck style={{padding:'12px', height:'275px'}}>
           <Card>           
             <CardBody>
-                <label style={{ marginLeft: 90 }}> Create a checklist for today's  meal delivery</label>
-            <div>
-             <Button onClick style={{ background: '#599BE9' }}>Create</Button>
-            </div>
+              <label style={{ marginLeft: 90 }}> Create a checklist for today's  meal delivery</label>
+              <div>
+                <Button onClick style={{ background: '#599BE9' }}>Create</Button>
+              </div>
             </CardBody>
           </Card>
-
-
-          <Card>            
-            <CardBody>
-            </CardBody>
-          </Card>
-
         </CardDeck>
-
       </Container>
   );
 }
