00cfbe56ca77e083de9b7567d83c679cc5a3830e
yelghom <yousra.elghomari@gmail.com>
2019-04-12 02:20:13 +0200

    display meal name, description and delivery status #187

--- frontend/src/component/delivery/DisplayDelivery.js
+++ frontend/src/component/delivery/DisplayDelivery.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
 import {
-  Container, Table
+  Container, Card, CardBody, Table
 } from 'reactstrap';
 class DisplayDelivery extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      apiObject: this.props.data,
+      displayLimit: 10,
+    }
+  }
   
   render() {
     return(
       <Container className="text-center">
         <h1>Delivery Checklist</h1>
+        {this.state.apiObject.consumerList.map(x =>
+
+          <div style={{ padding: '12px' }}>
+
+            <Card style={{ background: '#d3d3d3' }}>
+              <CardBody >
+                <div style={{ padding:'12px', maxWidth:'1800px', maxHeight:'275px', overflow:'scroll'}}>
+                  <Table bordered condensed>
+                    <thead style={{background: '#599BE9',color:'white'}}>
+                    <th className="text-center" >Name</th>
+                    <th className="text-center" >Description </th>
+                    <th className="text-center" >Delivery Status</th>
+                    </thead>
+                    <tbody style={{background:'#d3d3d3'}}>
+
+                    {
+                      x.mealList.map(x =>
+                        <tr>
+                          <td>{x.name}</td>
+                          <td>{x.description.toString()}</td>
+                          <td>{"true"}</td>
+                        </tr>
+                      )}
+                    </tbody>
+                  </Table>
+                </div>
+              </CardBody>
+            </Card>
+          </div>
+        )}
       </Container>
     )
   }
