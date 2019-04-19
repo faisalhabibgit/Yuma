1d0977f0ab01556040534555277baafdaac37ca1
Nathys <nathys@Nathyss-MacBook-Pro.local>
2019-04-19 13:49:06 -0400

    initial feature

---
+++ frontend/src/component/Blog.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+    Container
+} from 'reactstrap';
+
+
+class Blog extends Component {
+
+    render() {
+        return (
+            <Container>
+                <h1>Hi</h1>
+            </Container>
+        );
+    }
+
+
+}
+
+export default Blog;
