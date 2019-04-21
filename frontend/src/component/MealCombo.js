3c83736a964d9aa0c86dd8c85b8a6fbc3aa15cd1
yelghom <yousra.elghomari@gmail.com>
2019-04-21 02:09:02 +0200

    display only ONE combination #188

--- frontend/src/component/MealCombo.js
+++ frontend/src/component/MealCombo.js
@@ -6,8 +6,6 @@ import {
 import Modal from 'react-modal';
 import Poster from '../middleware/Poster';
 import ComboOne from './mealcombo/ComboOne';
-import ComboTwo from './mealcombo/ComboTwo';
-import ComboThree from './mealcombo/ComboThree';
 import Retriever from '../middleware/Retriever';
 import CustomLogging from "../CustomLogging";
 
@@ -28,8 +26,6 @@ class MealCombo extends Component {
     this.openModal = this.openModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
     this.handleModalChange1 = this.handleModalChange1.bind(this);
-    this.handleModalChange2 = this.handleModalChange2.bind(this);
-    this.handleModalChange3 = this.handleModalChange3.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleDownloadCSV = this.handleDownloadCSV.bind(this);
    // this.toggle = this.toggle.bind(this);
@@ -70,19 +66,7 @@ class MealCombo extends Component {
     CustomLogging.info("Combo One content opened","MealCombo");
     this.setState({ ModalContent: <ComboOne /> })
   }
-
-  handleModalChange2() {
-    this.openModal();
-    CustomLogging.info("Combo Two content opened","MealCombo");
-    this.setState({ ModalContent: <ComboTwo /> })
-  }
-
-  handleModalChange3() {
-    this.openModal();
-    CustomLogging.info("Combo Three content opened","MealCombo");
-    this.setState({ ModalContent: <ComboThree /> })
-  }
-
+  
   
   handleSubmit(e) {
     CustomLogging.info("Selected Combo"+e.target.id,"MealCombo");
@@ -124,9 +108,9 @@ handelSelect(){
       <div>
         <CardDeck style={{ padding: '15px', maxHeight: '200px' }}>
 
-          <Card>
+          <Card body outline color="primary">
             <CardBody className="text-center">
-              <h4> Meal Combo #1</h4>
+              <h4> Weekly Meal Combination</h4>
               <div>
                 <Button variant="secondary" onClick={this.handleModalChange1}>View Full Combination Report</Button>
               </div>
@@ -138,36 +122,6 @@ handelSelect(){
               </div> 
             </CardBody>
           </Card>
-          <Card>
-            <CardBody className="text-center">
-              <h4> Meal Combo #2</h4>
-              <div>
-                <Button variant="secondary" onClick={this.handleModalChange2}>View Full Combination Report</Button>
-              </div>
-              <div style={{ padding: '15px' }}>
-                <Button 
-                        style={{background: this.selectColor(2)}} onClick={() => {this.toggle(2)}}
-                       // onClick={this.handelSelect}
-                        id = '2' type="submit">Select</Button>
-              </div>
-            </CardBody>
-          </Card>
-
-          <Card>
-            <CardBody className="text-center">
-              <h4> Meal Combo #3</h4>
-              <div>
-                <Button variant="secondary" onClick={this.handleModalChange3}>View Full Combination Report</Button>
-              </div>
-              <div style={{ padding: '15px' }}>
-                <Button 
-                        style={{background: this.selectColor(3)}}  onClick={() => {this.toggle(3)}}
-                        //onClick={this.handleSubmit}
-                        id='3' type="submit">Select</Button>
-              </div>
-            </CardBody>
-          </Card>
-
         </CardDeck>
 
         <Modal
@@ -175,7 +129,7 @@ handelSelect(){
           onRequestClose={this.closeModal}
           style={{ width: '1000px', height: '1000px' }}
         >
-          <button onClick={this.closeModal}>close</button>
+          <button  onClick={this.closeModal}>close</button>
           <div>{this.state.ModalContent}</div>
         </Modal>
         <div>{this.state.downloadCSV}</div>
