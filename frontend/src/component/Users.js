4f1e71ac030289f0ace04afd20e08a94ff20a290
yelghom <yousra.elghomari@gmail.com>
2019-04-21 02:08:12 +0200

    Display Users in new Page Users #188

---
+++ frontend/src/component/Users.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import ApiToken from "../middleware/ApiToken";
+
+import {
+  Card, CardHeader,  CardBody,
+  CardDeck, Button
+} from 'reactstrap';
+import AvailableMeals from "./AvailableMeals";
+import DisplayMeals from "./DisplayMeals";
+import DisplayUsers from "./DisplayUsers";
+import MealCombo from "./MealCombo";
+import CustomLogging from '../CustomLogging';
+import DisplayProteinScore from "./DisplayProteinScore";
+
+class Users extends Component {
+
+
+  constructor(props) {
+    super(props);
+
+    this.checkAuthenticated();
+  }
+
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+
+
+
+  checkAuthenticated(){
+
+    const apiToken = new ApiToken();
+    if (!apiToken.isAuthenticated()) {
+      CustomLogging.error('Check Authentification Dashboard: FAIL','dashboard');
+      this.props.history.push(`/Login`)
+    } else {
+      CustomLogging.info('Check Authentification Dashboard: PASS','Dashboard');
+    }
+
+  }
+
+  render() {
+    return (
+      
+      <CardDeck data-test="users-card" style={{padding:'12px', height:'620px'}}>
+          <Card body outline color="primary">
+            <CardHeader  className="text-center" style={{background: '#B9C5D5'}}>
+              <div>
+                <Button variant="secondary"  onClick={this.handleModalChange3}>Update User</Button>
+              </div>
+            </CardHeader>
+            <CardBody>
+              <DisplayUsers />
+            </CardBody>
+          </Card>
+
+        </CardDeck>
+
+    );
+
+  }
+}
+
+
+export default Users;
